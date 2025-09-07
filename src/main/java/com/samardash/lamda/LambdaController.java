bdf422d5f4c270ebb09405b4d1fcf4ccaea0b5af
Samar Dash <12dit152@gmail.com>
2025-09-07 12:21:49 +0100

    Update logging level to INFO, enhance /hello endpoint response, and upgrade Spring Boot version to 3.5.0

--- src/main/java/com/samardash/lamda/LambdaController.java
+++ src/main/java/com/samardash/lamda/LambdaController.java
@@ -7,14 +7,28 @@ import org.springframework.web.bind.annotation.RestController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 @RestController
 @RequestMapping("/api/v1/")
 public class LambdaController {
     private static final Logger log = LoggerFactory.getLogger(LambdaController.class);
 
     @RequestMapping(value = "/hello", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String hello() {
-       log.info("Received request for /hello endpoint");
-        return "{\"message\": \"Hello from AWS Lambda\"}";
+    public String hello(ObjectMapper objectMapper) {
+        log.info("Received request for /hello endpoint");
+
+        ObjectNode response = objectMapper.createObjectNode();
+        response.put("Message", "Hello from AWS Lambda");
+        response.put("Developer", "Samar Dash");
+        response.put("Version", "1.0.0");
+        response.put("Home Page", "https://samardash.com");
+        response.put("Href", "https://api.samardash.com/api/v1/hello");
+        response.put("Flow", "API Gateway -> Lambda -> Spring Boot");
+        response.put("Notes", "This is a sample Spring Boot application running on AWS Serverless environment.");
+
+        log.debug("Response: {}", response);
+        return response.toPrettyString();
     }
 }
